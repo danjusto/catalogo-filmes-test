4560067c9bc45cafb6284fb380f13d8d78113eaf
Daniel Justo <justodaniel29@gmail.com>
2023-11-28 14:00:20 -0300

    swagger

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { useContainer } from 'class-validator';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -13,6 +14,15 @@ async function bootstrap() {
         }),
     );
     useContainer(app.select(AppModule), { fallbackOnErrors: true });
+    const config = new DocumentBuilder()
+        .setTitle('Movies catalog')
+        .setDescription('API for a test')
+        .setVersion('1.0')
+        .addTag('movies')
+        .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+
     await app.listen(3000);
 }
 bootstrap();
